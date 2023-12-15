bbcbf850ea6a78ced850a3cb98fd3bbaec3edec8
jongmin <jongmin0919@github.com>
2023-12-15 13:59:36 +0900

    헤더 타이틀 및 nav 완료

---
+++ src/components/navBar.jsx
@@ -0,0 +1,13 @@
+import '../componentCss/navBar.css'
+
+const NavLeft = () => {
+  return (
+    <ul className="nav-list">
+      <li><a href="#">home</a></li>
+      <li><a href="#">sign in</a></li>
+      <li><a href="#">sign up</a></li>
+    </ul>
+  );
+}
+
+export default NavLeft;
---
+++ src/components/navLogo.jsx
@@ -0,0 +1,11 @@
+import '../componentCss/navLogo.css'
+
+const NavLogo = () => {
+  return (
+    <div className="Nav-logo">
+      <p>conduit</p>
+    </div>
+  );
+}
+
+export default NavLogo;
